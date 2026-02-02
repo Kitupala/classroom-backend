9438874267d3b45b76f72570568f6f109654f8b5
Kitupala <kimmo.tuupanen@gmail.com>
2026-02-02 16:41:09 +0200

    Integrate BetterAuth for user authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,38 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import {db} from "../db";
+import * as schema from "../db/schema/auth";
+
+
+export const auth = betterAuth({
+    secret: process.env.BETTER_AUTH_SECRET!,
+    baseURL: process.env.BETTER_AUTH_BASE_URL!,
+    trustedOrigins: [process.env.FRONTEND_URL!],
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    account: {
+        fields: {
+            password: "passwordHash"
+        }
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                required: true,
+                default: "student",
+                input: true
+            },
+            imageCldPubId: {
+                type: "string",
+                required: false,
+                input: true
+            }
+        }
+    }
+});
